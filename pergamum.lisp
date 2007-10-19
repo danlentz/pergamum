0d1f53cd669d44ce81071762770ea99a752873d8
Samium Gromoff <deepfire@betelheise.deep.net>
2007-10-19 05:29:14 +0400

    Debug OP-PARAMETER-DESTRUCTURER and DEFINE-EVALUATION-DOMAIN. Implement QUOTE-LISTS-FILTER.

--- pergamum.lisp
+++ pergamum.lisp
@@ -1,35 +1,45 @@
 (defpackage pergamum
   (:use :common-lisp :alexandria)
   (:export
+   #:quote-lists-filter
    #:op-parameter-destructurer
    #:define-evaluation-domain
    ))
 
 (in-package :pergamum)
 
-(defmacro op-parameter-destructurer ((op &rest params) form &body body)
-  `(let ((,op ,(if (consp form) (first form) form))
-	 (,params ,@(unless (consp form) `(rest ,form))))
-     ,@body))
+(defun quote-lists-filter (form)
+  "If FORM is a list, return it quoted; identity-transform otherwise."
+  (if (listp form)
+      (list 'quote form)
+      form))
+
+(defmacro op-parameter-destructurer ((op params) form &body body)
+  (once-only (form)
+    `(let ((,op (if (consp ,form) (first ,form) ,form))
+	   (,params (when (consp ,form) (rest ,form))))
+       ,@body)))
 
 ;; This macro belongs to the wider world.
 (defmacro define-evaluation-domain (domain-name (&rest evaluation-result-vars))
-  (with-gensyms (table-name name lambda-list type op params body)
+  (with-gensyms (table-name name lambda-list)
     `(progn
-       (defvar ,table-name (make-hash-table))
+       (defparameter ,table-name (make-hash-table))
        
-       (defmacro ,(format-symbol (symbol-package domain-name) "DEFINE-~S" domain-name) (,name ,lambda-list &body ,body)
-	 `(setf (gethash ',,domain-name ,,table-name)
-		(lambda ,,lambda-list ,@,body)))
+       (defmacro ,(format-symbol (symbol-package domain-name) "DEFINE-~S" domain-name) (,name ,lambda-list &body body)
+	 `(setf (gethash ',,name ,',table-name)
+		(lambda ,,lambda-list ,@body)))
 
-       (defmacro ,(format-symbol (symbol-package domain-name) "APPLY-BIND-~S" domain-name) ((,@evaluation-result-vars) ,type &body ,body)
+       (defmacro ,(format-symbol (symbol-package domain-name) "APPLY-BIND-~S" domain-name) ((,@evaluation-result-vars) type &body body)
 	 "Given a parser op invocation, deliver the appropriately specialised op 'guts'."
-	 (op-parameter-destructurer (,op ,params) ,type
-	   `(multiple-value-bind (,,@evaluation-result-vars) (apply (gethash ,,op ,,table-name) ,,params)
-	      ,@,body)))
+	 (with-gensyms (op params)
+	   `(op-parameter-destructurer (,op ,params) ,type
+	      (multiple-value-bind (,,@evaluation-result-vars) (apply (gethash ,op ,',table-name) ,params)
+		,@body))))
 
-       (defmacro ,(format-symbol (symbol-package domain-name) "FUNCALL-BIND-~S" domain-name) ((,@evaluation-result-vars) ,type &body ,body)
+       (defmacro ,(format-symbol (symbol-package domain-name) "FUNCALL-BIND-~S" domain-name) ((,@evaluation-result-vars) type &body body)
 	 "Given a parser op invocation, deliver the appropriately specialised op 'guts'."
-	 (op-parameter-destructurer (,op ,params) ,type
-	   `(multiple-value-bind (,,@evaluation-result-vars) (funcall (gethash ,,op ,,table-name) ,@,params)
-	      ,@,body))))))
+	 (with-gensyms (op params)
+	   `(op-parameter-destructurer (,op ,params) ,type
+	      (multiple-value-bind (,,@evaluation-result-vars) (funcall (gethash ,op ,',table-name) ,@params)
+		,@body)))))))

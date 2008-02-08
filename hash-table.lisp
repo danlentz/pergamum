18f2a9957c8b3572390b3ae5a31a80454807a911
Samium Gromoff <deepfire@elvees.com>
2008-02-08 05:35:21 +0300

    Override MAPHASH-{KEYS,VALUES} to accept :TEST, :TEST-NOT and :KEY.

--- hash-table.lisp
+++ hash-table.lisp
@@ -31,3 +31,37 @@
 (defun hash-table-key-present-p (hash-table key)
   "Given a HASH-TABLE and a KEY, return an injector function accepting KEY and VALUE."
   (nth 1 (gethash key hash-table)))
+
+(defmacro apply-key (key element)
+  `(if ,key
+       (funcall ,key ,element)
+       ,element))
+
+(defmacro satisfies-the-test (elt)
+  (with-gensyms (key-tmp)
+    `(let ((,key-tmp (apply-key key ,elt)))
+       (cond (testp (funcall test ,key-tmp))
+             (notp (not (funcall test-not ,key-tmp)))
+             (t t)))))
+
+(declaim (inline maphash-keys))
+(defun maphash-keys (function table &key key (test nil testp) (test-not nil notp))
+  "Like MAPHASH, but calls FUNCTION with each key in the hash table TABLE."
+  (when (and testp notp)
+    (error ":TEST and :TEST-NOT were both supplied."))
+  (maphash (lambda (k v)
+             (declare (ignore v))
+             (when (satisfies-the-test k)
+               (funcall function k)))
+           table))
+
+(declaim (inline maphash-values))
+(defun maphash-values (function table &key key (test nil testp) (test-not nil notp))
+  "Like MAPHASH, but calls FUNCTION with each value in the hash table TABLE."
+  (when (and testp notp)
+    (error ":TEST and :TEST-NOT were both supplied."))
+  (maphash (lambda (k v)
+             (declare (ignore k))
+             (when (satisfies-the-test v)
+               (funcall function v)))
+           table))

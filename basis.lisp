005e3d9b4ba0965b57350874861bea79b98ffb65
Samium Gromoff <deepfire@betelheise.feelingofgreen.ru>
2008-12-25 18:12:45 +0300

    Fix IF-LET* docstring and definition.

--- basis.lisp
+++ basis.lisp
@@ -29,11 +29,7 @@
   "Creates new variable bindings, and conditionally executes either THEN-FORM
 or ELSE-FORM. ELSE-FORM defaults to NIL.
 
-BINDINGS must be either single binding of the form:
-
- (variable initial-form)
-
-or a list of bindings of the form:
+BINDINGS must be a list of bindings of the form:
 
  ((variable-1 initial-form-1)
   (variable-2 initial-form-2)
@@ -46,15 +42,24 @@ previously bound by the IF-LET*.
 
 If all variables are true after the bindings are complete, the THEN-FORM is
 executed with the bindings in effect, otherwise the ELSE-FORM is executed with
-the bindings in effect."
-  (let* ((binding-list (if (and (consp bindings) (symbolp (car bindings)))
-                           (list bindings)
-                           bindings))
-         (variables (mapcar #'car binding-list)))
-    `(let* ,binding-list
-       (if (and ,@variables)
-           ,then-form
-           ,else-form))))
+the bindings NOT in effect."
+  (with-gensyms (%if-let* block else-tag)
+    `(macrolet ((,%if-let* (rec-bindings)
+                  (let* ((,%if-let* ',%if-let*)
+                         (,block ',block)
+                         (,else-tag ',else-tag))
+                    (destructuring-bind (first-binding &rest rest) rec-bindings
+                      `(let (,first-binding)
+                         (if ,(car first-binding)
+                             ,(if (endp rest)
+                                  `(return-from ,,block ,',then-form)
+                                  `(,,%if-let* ,rest))
+                             (go ,,else-tag)))))))
+       (block ,block
+         (tagbody
+            (,%if-let* ,bindings)
+            ,else-tag
+            (return-from ,block ,else-form))))))
 
 (defmacro case-let (binding &body clauses)
   "BINDING is a single variable binding established for CLAUSES,

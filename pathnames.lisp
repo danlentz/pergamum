a746878c42ab61d58bd65abc4cbac0a7a55d9f32
Samium Gromoff <deepfire@betelheise.feelingofgreen.ru>
2008-12-26 02:55:37 +0300

    Interpret NIL as the DIRECTORY-PATHNAME to SUB{FILE,DIRECTORY}{,*} as *D-P-D*

--- pathnames.lisp
+++ pathnames.lisp
@@ -4,20 +4,20 @@
 (defun subfile (directory-pathname sub &rest make-pathname-keywords)
   "Return a file pathname with name SUB in DIRECTORY-PATHNAME.
    MAKE-PATHNAME-KEYWORDS are passed to MAKE-PATHNAME."
-  (merge-pathnames (apply #'make-pathname :directory `(:relative ,@(butlast sub)) :name (lastcar sub) make-pathname-keywords) directory-pathname))
+  (merge-pathnames (apply #'make-pathname :directory `(:relative ,@(butlast sub)) :name (lastcar sub) make-pathname-keywords) (or directory-pathname *default-pathname-defaults*)))
 
 (defun subfile* (directory-pathname &rest sub)
   "Return a file pathname with name SUB in DIRECTORY-PATHNAME."
-  (merge-pathnames (make-pathname :directory `(:relative ,@(butlast sub)) :name (lastcar sub)) directory-pathname))
+  (merge-pathnames (make-pathname :directory `(:relative ,@(butlast sub)) :name (lastcar sub)) (or directory-pathname *default-pathname-defaults*)))
 
 (defun subdirectory (directory-pathname sub &rest make-pathname-keywords)
   "Return a subdirectory pathname with name SUB in DIRECTORY-PATHNAME.
    MAKE-PATHNAME-KEYWORDS are passed to MAKE-PATHNAME."
-  (merge-pathnames (apply #'make-pathname :directory `(:relative ,@sub) make-pathname-keywords) directory-pathname))
+  (merge-pathnames (apply #'make-pathname :directory `(:relative ,@sub) make-pathname-keywords) (or directory-pathname *default-pathname-defaults*)))
 
 (defun subdirectory* (directory-pathname &rest sub)
   "Return a subdirectory pathname with name SUB in DIRECTORY-PATHNAME."
-  (merge-pathnames (make-pathname :directory `(:relative ,@sub)) directory-pathname))
+  (merge-pathnames (make-pathname :directory `(:relative ,@sub)) (or directory-pathname *default-pathname-defaults*)))
 
 (defun flatten-path-list (path &optional absolute)
   "Transform a list of strings in PATH into a string constituting of 

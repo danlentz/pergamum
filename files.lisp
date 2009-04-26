b1ec01ebace2bb5be092d268c07828f270c33007
Samium Gromoff <deepfire@gemini.deep>
2009-04-27 03:23:10 +0400

    Allow a convenience shortcut in WITHIN-DIRECTORY.

--- files.lisp
+++ files.lisp
@@ -20,39 +20,45 @@
 
 (defsetf posix-working-directory set-posix-working-directory)
 
-(defmacro within-directory ((directory-form &key (as (gensym)) (lisp t) (posix t) (if-exists :continue) (if-does-not-exist :error)) &body body)
+(defmacro within-directory (directory-spec &body body)
   "Execute BODY with the idea of current directory changed to the
-   value of DIRECTORY-FORM, which must evaluate to a string, which is 
-   optionally captured in the binding of a variable named by AS.
+   value specified by DIRECTORY-SPEC, which is either a lambda list
+   with one mandatory form (interpreted as a directory specifier)
+   and some optional keywords, or an atom directory specifier,
+   i.e. either a string or a symbol which mush evaluate to a string.
+
+   The value of the directory specifier is optionally captured 
+   in the binding of a variable named by the AS keyword parameter.
 
    Defined keywords:
     :AS - a symbol, optionally specifying the name of the variable
-          to be bound to the value of DIRECTORY-FORM
+          to be bound to the value of the directory specifier
     :LISP - a boolean, indicating a request to bind *D-P-D*
     :POSIX - a boolean, indicating a request to change POSIX's idea
              of the current directory
     :IF-EXISTS - one of :ERROR or :CONTINUE
     :IF-DOES-NOT-EXIST - one of :ERROR or :CREATE
    See the manual for details."
-  (flet ((wrap-body (dirsym oldsym body)
-           `(let (,@(when posix `((,oldsym (posix-working-directory))))
-                  ,@(when lisp `((*default-pathname-defaults* (parse-namestring ,dirsym)))))
-              ,@(if posix
-                    `((set-posix-working-directory ,dirsym)
-                      (unwind-protect (progn ,@body)
-                        (set-posix-working-directory ,oldsym)))
-                    body))))
-    (with-gensyms (old)
-      `(let ((,as ,directory-form))
-         (if (directory-exists-p ,as)
-             ,(ecase if-exists
-                     (:continue (wrap-body as old body))
-                     (:error `(error 'pathname-busy :pathname ,as)))
-             ,(ecase if-does-not-exist
-                     (:create `(progn
-                                 (ensure-directories-exist ,as)
-                                 ,(wrap-body as old body)))
-                     (:error `(error 'pathname-not-present :pathname ,as))))))))
+  (destructuring-bind (directory-form &key (as (gensym)) (lisp t) (posix t) (if-exists :continue) (if-does-not-exist :error)) (ensure-list directory-spec)
+    (flet ((wrap-body (dirsym oldsym body)
+             `(let (,@(when posix `((,oldsym (posix-working-directory))))
+                    ,@(when lisp `((*default-pathname-defaults* (parse-namestring ,dirsym)))))
+                ,@(if posix
+                      `((set-posix-working-directory ,dirsym)
+                        (unwind-protect (progn ,@body)
+                          (set-posix-working-directory ,oldsym)))
+                      body))))
+      (with-gensyms (old)
+        `(let ((,as ,directory-form))
+           (if (directory-exists-p ,as)
+               ,(ecase if-exists
+                       (:continue (wrap-body as old body))
+                       (:error `(error 'pathname-busy :pathname ,as)))
+               ,(ecase if-does-not-exist
+                       (:create `(progn
+                                   (ensure-directories-exist ,as)
+                                   ,(wrap-body as old body)))
+                       (:error `(error 'pathname-not-present :pathname ,as)))))))))
 
 (defun file-as-vector (filename &rest rest &key (element-type '(unsigned-byte 8)) &allow-other-keys)
   "Return contents of FILENAME as simple vector with element type (UNSIGNED-BYTE 8)."

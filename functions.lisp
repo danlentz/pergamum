85ec00d5df3f69018dbea84d100bfaabb319ee5b
Samium Gromoff <deepfire@elvees.com>
2009-07-02 01:14:26 +0400

    DEFINE-EXECUTE-WITH-SPECIAL

--- functions.lisp
+++ functions.lisp
@@ -123,3 +123,21 @@
 
 (defun and-p (a b)
   (and a b))
+
+(defmacro define-execute-with-special (name)
+  (let ((execute-with-fn-name (format-symbol (symbol-package name) "EXECUTE-WITH-~A" name))
+        (with-macro-name (format-symbol (symbol-package name) "WITH-~A" name))
+        (special-var-name (format-symbol (symbol-package name) "*~A*" name)))
+    `(progn
+       (defun ,execute-with-fn-name (,name fn)
+         ,(format nil "Call FN within dynamic context where ~A is bound to ~A.
+FN must be a function of no arguments." special-var-name name)
+         (let ((,special-var-name ,name))
+           (declare (special ,special-var-name))
+           (funcall fn)))
+
+       (defmacro ,with-macro-name (,name &body body)
+         ,(format nil "Execute BODY within dynamic context where ~A is bound to
+the result of evaluation of ~A." special-var-name name)
+         
+         `(,',execute-with-fn-name ,,name (lambda () ,@body))))))

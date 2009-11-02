585ebc74631d96f30ae29b4a6a75b4ddcea8f73e
Samium Gromoff <_deepfire@feelingofgreen.ru>
2009-11-03 02:17:33 +0300

    Extend WITHIN-DIRECTORY somewhat.

--- files.lisp
+++ files.lisp
@@ -22,23 +22,29 @@
 
 (defmacro within-directory (directory-spec &body body)
   "Execute BODY with the idea of current directory changed to the
-   value specified by DIRECTORY-SPEC, which is either a lambda list
-   with one mandatory form (interpreted as a directory specifier)
-   and some optional keywords, or an atom directory specifier,
-   i.e. either a string or a symbol which mush evaluate to a string.
+value specified by DIRECTORY-SPEC, which is either a lambda list
+with one mandatory form (interpreted as a directory specifier)
+and some optional keywords, or an atom directory specifier,
+i.e. either a string or a symbol which mush evaluate to a string.
 
-   The value of the directory specifier is optionally captured 
-   in the binding of a variable named by the AS keyword parameter.
+The value of the directory specifier is optionally captured 
+in the binding of a variable named by the AS keyword parameter.
 
-   Defined keywords:
-    :AS - a symbol, optionally specifying the name of the variable
-          to be bound to the value of the directory specifier
-    :LISP - a boolean, indicating a request to bind *D-P-D*
-    :POSIX - a boolean, indicating a request to change POSIX's idea
-             of the current directory
-    :IF-EXISTS - one of :ERROR or :CONTINUE
-    :IF-DOES-NOT-EXIST - one of :ERROR or :CREATE
-   See the manual for details."
+Defined keywords:
+ :AS - a symbol, optionally specifying the name of the variable
+       to be bound to the value of the directory specifier
+ :LISP - a boolean, indicating a request to bind *D-P-D*
+ :POSIX - a boolean, indicating a request to change POSIX's idea
+          of the current directory
+ :IF-EXISTS - one of :ERROR or :CONTINUE
+ :IF-DOES-NOT-EXIST - one of :ERROR or :CREATE
+See the manual for details.
+
+Within the lexical context established by the WITHIN-DIRECTORY form,
+(DIRECTORY-EXISTED-P) evaluates to a boolean value representing
+whether the directory had to be created, when IF-DOES-NOT-EXIST
+is :CREATE. (DIRECTORY-CREATED-P) evaluates to the negation of that
+value."
   (destructuring-bind (directory-form &key (as (gensym)) (lisp t) (posix t) (if-exists :continue) (if-does-not-exist :error)) (ensure-list directory-spec)
     (flet ((wrap-body (dirsym oldsym body)
              `(let (,@(when posix `((,oldsym (posix-working-directory))))
@@ -48,17 +54,20 @@
                         (unwind-protect (progn ,@body)
                           (set-posix-working-directory ,oldsym)))
                       body))))
-      (with-gensyms (old)
-        `(let ((,as ,directory-form))
-           (if (directory-exists-p ,as)
-               ,(ecase if-exists
-                       (:continue (wrap-body as old body))
-                       (:error `(error 'pathname-busy :pathname ,as)))
-               ,(ecase if-does-not-exist
-                       (:create `(progn
-                                   (ensure-directories-exist ,as)
-                                   ,(wrap-body as old body)))
-                       (:error `(error 'pathname-not-present :pathname ,as)))))))))
+      (with-gensyms (old existsp)
+        `(let ((,as ,directory-form)
+               (,existsp (directory-exists-p ,as)))
+           (macrolet ((directory-created-p () `(not ,existsp))
+                      (directory-existed-p () ,existsp))
+             (if ,existsp
+                 ,(ecase if-exists
+                         (:continue (wrap-body as old body))
+                         (:error `(error 'pathname-busy :pathname ,as)))
+                 ,(ecase if-does-not-exist
+                         (:create `(progn
+                                     (ensure-directories-exist ,as)
+                                     ,(wrap-body as old body)))
+                         (:error `(error 'pathname-not-present :pathname ,as))))))))))
 
 (defun file-as-vector (filename &rest rest &key (element-type '(unsigned-byte 8)) position &allow-other-keys)
   "Return contents of FILENAME, starting from POSITION, as simple vector

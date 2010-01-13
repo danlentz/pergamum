dbae36ff345b20a7d24f70c9e187cc6a67a25692
Samium Gromoff <deepfire@elvees.com>
2010-01-13 15:28:15 +0300

    Flatten package declaration.

--- package.lisp
+++ package.lisp
@@ -36,85 +36,226 @@
    ;; classes.lisp
    #:define-protocol-class
    ;; objects.lisp
-   #:copy-slots #:slot-value* #:define-print-object-method #:primary-method-not-required #:most-specific-last
+   #:copy-slots
+   #:slot-value*
+   #:define-print-object-method
+   #:primary-method-not-required
+   #:most-specific-last
    ;; conditions.lisp
-   #:*debug-condition* #:dsignal #:derror
-   #:backtrace #:backtrace-as-list #:print-backtrace-frame
-   #:with-condition-recourses #:call-next-recourse-and-retry
-   #:make-condition-raiser #:make-error-raiser 
-   #:with-retry-restarts #:with-ignore-restart
+   #:*debug-condition*
+   #:dsignal
+   #:derror
+   #:backtrace
+   #:backtrace-as-list
+   #:print-backtrace-frame
+   #:with-condition-recourses
+   #:call-next-recourse-and-retry
+   #:make-condition-raiser
+   #:make-error-raiser 
+   #:with-retry-restarts
+   #:with-ignore-restart
    #:make-fixed-restarter
-   #:with-condition-restart-binding #:with-maybe-just-printing-conditions #:with-condition-printing #:with-error-resignaling #:with-collected-conditions
+   #:with-condition-restart-binding
+   #:with-maybe-just-printing-conditions
+   #:with-condition-printing
+   #:with-error-resignaling
+   #:with-collected-conditions
    #:with-recorded-status
    #:condition-bind-default
-   #:define-reported-condition #:report-simple-condition #:define-simple-error
+   #:define-reported-condition
+   #:report-simple-condition
+   #:define-simple-error
    #:simple-condition-reporter
    ;; conditions.lisp :: canned
-   #:redefinition #:simple-redefinition #:bad-redefinition #:warn-redefinition
-   #:missing-implementation #:not-implemented
+   #:redefinition
+   #:simple-redefinition
+   #:bad-redefinition
+   #:warn-redefinition
+   #:missing-implementation
+   #:not-implemented
    ;; functions.lisp
-   #:compose* #:reduce*
-   #:feq #:feql #:fequal #:fequalp #:f=
-   #:latch #:bukkake-combine #:maybe #:maybecall #:xform #:xform-if #:xform-if-not #:apply/find-if
-   #:iterate-until #:collect-until #:or-p #:and-p
-   #:function-insn-vector #:disassemble-insn-vector
+   #:compose*
+   #:reduce*
+   #:feq
+   #:feql
+   #:fequal
+   #:fequalp
+   #:f=
+   #:latch
+   #:bukkake-combine
+   #:maybe
+   #:maybecall
+   #:xform
+   #:xform-if
+   #:xform-if-not
+   #:apply/find-if
+   #:iterate-until
+   #:collect-until
+   #:or-p
+   #:and-p
+   #:function-insn-vector
+   #:disassemble-insn-vector
    ;; lists.lisp
-   #:nfsubst #:make-queue #:enqueue #:dequeue #:queue-contents #:queue-empty-p #:mapqueue #:mapcons #:unzip #:diff-lists
-   #:plist-difference #:plist-intersection #:set-differencef #:nset-differencef #:set-intersectionf #:nset-intersectionf
-   #:maptree #:map-list-tree
+   #:nfsubst
+   #:make-queue
+   #:enqueue
+   #:dequeue
+   #:queue-contents
+   #:queue-empty-p
+   #:mapqueue
+   #:mapcons
+   #:unzip
+   #:diff-lists
+   #:plist-difference
+   #:plist-intersection
+   #:set-differencef
+   #:nset-differencef
+   #:set-intersectionf
+   #:nset-intersectionf
+   #:maptree
+   #:map-list-tree
    ;; mop.lisp
-   #+sbcl #:rename-class-slot #:slot-definition-documentation
+   #+sbcl #:rename-class-slot
+   #:slot-definition-documentation
    ;; numbers.lisp
-   #:power-of-2-p #:ilog2-ceiling #:ilog2-cover #:bcd-integer-length #:decode-bcd #:bisect #:split-number
-   #:with-alignment #:map-alignment-left #:map-alignment-right #:operate-on-extremity
+   #:power-of-2-p
+   #:ilog2-ceiling
+   #:ilog2-cover
+   #:bcd-integer-length
+   #:decode-bcd
+   #:bisect
+   #:split-number
+   #:with-alignment
+   #:map-alignment-left
+   #:map-alignment-right
+   #:operate-on-extremity
    ;; pergamum.lisp
    #:with-condition-printing
-   #:nand #:nor #:andf #:orf #:notf #:xorf
-   #:quote-when #:quote-if-non-self-evaluating
+   #:nand
+   #:nor
+   #:andf
+   #:orf
+   #:notf
+   #:xorf
+   #:quote-when
+   #:quote-if-non-self-evaluating
    #:op-parameter-destructurer
-   #:order #:order-funcalling
+   #:order
+   #:order-funcalling
    #:vector-=
-   #:ensure-destructurisation #:destructure-binding-form-body #:destructure-def-body
+   #:ensure-destructurisation
+   #:destructure-binding-form-body
+   #:destructure-def-body
    #:prepend
-   #:emit-let #:emit-lambda #:emit-named-lambda #:with-named-lambda-emission #:emit-defun #:with-defun-emission
+   #:emit-let
+   #:emit-lambda
+   #:emit-named-lambda
+   #:with-named-lambda-emission
+   #:emit-defun
+   #:with-defun-emission
    #:emit-declarations
-   #:with-measured-time-lapse #:with-time-lapse-measure #:with-measured-performance
+   #:with-measured-time-lapse
+   #:with-time-lapse-measure
+   #:with-measured-performance
    ;; lambda-lists.lisp
    #:&mandatory
-   #:map-lambda-list #:map-lambda-list-bindings-actuals #:map-lambda-list-defaults-actuals
-   #:lambda-list-binds #:lambda-list-1
+   #:map-lambda-list
+   #:map-lambda-list-bindings-actuals
+   #:map-lambda-list-defaults-actuals
+   #:lambda-list-binds
+   #:lambda-list-1
    ;; streams.lisp
-   #:stream-as-vector #:stream-as-string #:all-stream-forms
+   #:stream-as-vector
+   #:stream-as-string
+   #:all-stream-forms
    ;; pathnames.lisp
-   #:flatten-path-list #:fuse-downcased-string-path-list
-   #:subwild #:subfile #:subfile* #:subdirectory #:subdirectory* 
+   #:flatten-path-list
+   #:fuse-downcased-string-path-list
+   #:subwild
+   #:subfile
+   #:subfile*
+   #:subdirectory
+   #:subdirectory* 
    ;; files.lisp
-   #:pathname-busy #:pathname-not-present
-   #:posix-working-directory #:set-posix-working-directory
+   #:pathname-busy
+   #:pathname-not-present
+   #:posix-working-directory
+   #:set-posix-working-directory
    #:check-pathname-not-occupied
-   #:within-directory #:directory-created-p #:directory-existed-p #:maybe-within-directory
-   #:file-as-vector #:file-as-string #:file-line #:with-output-to-file
-   #:remove-file #:symlink-to-p #:make-symlink #:symlink-target-file #:ensure-symlink
+   #:within-directory
+   #:directory-created-p
+   #:directory-existed-p
+   #:maybe-within-directory
+   #:file-as-vector
+   #:file-as-string
+   #:file-line
+   #:with-output-to-file
+   #:remove-file
+   #:symlink-to-p
+   #:make-symlink
+   #:symlink-target-file
+   #:ensure-symlink
    ;; hash-table.lisp
-   #:hash-table-next #:hash-table-pop #:hash-table-itearate
-   #:xform-hash-table #:make-hash-table-injector #:hash-table-key-present-p #:maphash*
-   #:puthash-unique #:gethash-unique
-   #:define-subcontainer #:define-root-container #:with-container
-   #:container-condition #:container-missing-cell-error #:copy-hash-table-empty #:with-empty-hash-containers
+   #:hash-table-next
+   #:hash-table-pop
+   #:hash-table-itearate
+   #:xform-hash-table
+   #:make-hash-table-injector
+   #:hash-table-key-present-p
+   #:maphash*
+   #:puthash-unique
+   #:gethash-unique
+   #:define-subcontainer
+   #:define-root-container
+   #:with-container
+   #:container-condition
+   #:container-missing-cell-error
+   #:copy-hash-table-empty
+   #:with-empty-hash-containers
    ;; alignment.lisp
-   #:alignment-condition #:address-misalignment #:size-misalignment
-   #:aligned-p #:align-down #:align-up
-   #:aligned-4 #:aligned-16
-   #:check-size-alignment #:check-address-alignment
+   #:alignment-condition
+   #:address-misalignment
+   #:size-misalignment
+   #:aligned-p
+   #:align-down
+   #:align-up
+   #:aligned-4
+   #:aligned-16
+   #:check-size-alignment
+   #:check-address-alignment
    ;; extent.lisp
-   #:baseless-extent #:extent #:make-extent #:base #:size #:end #:extent-data #:inp #:intersectp
-   #:subextent* #:subextent-abs* #:subextent #:subextent-abs
-   #:split-extent #:with-split-extent
+   #:baseless-extent
+   #:extent
+   #:make-extent
+   #:base
+   #:size
+   #:end
+   #:extent-data
+   #:inp
+   #:intersectp
+   #:subextent*
+   #:subextent-abs*
+   #:subextent
+   #:subextent-abs
+   #:split-extent
+   #:with-split-extent
    #:extent-mask
-   #:rebase #:nrebase #:coerce-extent #:ncoerce-extent
-   #:extent-data-equalp #:extent-equalp
-   #:extent-spec #:extent #:print-extent-spec #:print-extent #:serialize-extent #:serialize-extent-list #:extent-reader #:read-extent-list
-   #:do-extent-spec-aligned-blocks #:with-aligned-extent-spec-pieces
+   #:rebase
+   #:nrebase
+   #:coerce-extent
+   #:ncoerce-extent
+   #:extent-data-equalp
+   #:extent-equalp
+   #:extent-spec
+   #:extent
+   #:print-extent-spec
+   #:print-extent
+   #:serialize-extent
+   #:serialize-extent-list
+   #:extent-reader
+   #:read-extent-list
+   #:do-extent-spec-aligned-blocks
+   #:with-aligned-extent-spec-pieces
    ;; u8-sequence.lisp
    #:u8-vector-wordle
    #:u8-vector-wordbe
